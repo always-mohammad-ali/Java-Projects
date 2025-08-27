d20ee0687debfd02f92d2f27d2f3c4c0bc21737d
Mohammad Ali <always.mohammad.ali@gmail.com>
2025-08-27 15:12:37 +0600

    initial java code push for the first time

---
+++ Hello World/Main.java
@@ -0,0 +1,15 @@
+
+
+public class Main{
+
+    
+    public static void main(String[] args){
+
+        System.out.print("Hello JAVA WORLD \n");
+        
+        System.out.println("You can use 'sout' as shortcut for system.out.println");
+
+        
+        // println means print line, so it will create new line of output man.
+    }
+}

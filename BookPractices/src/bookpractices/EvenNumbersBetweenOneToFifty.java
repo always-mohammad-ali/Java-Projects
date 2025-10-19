57c204cd038e55ee5fdbe51adf737bdbf9a270fd
Mohammad Ali <always.mohammad.ali@gmail.com>
2025-10-19 09:55:40 +0600

    Print all even numbers between 1 to 50

---
+++ BookPractices/src/bookpractices/EvenNumbersBetweenOneToFifty.java
@@ -0,0 +1,32 @@
+/*
+ 3. Print all even numbers between 1 and 50.
+4. Take an integer input n and calculate the sum of numbers from 1
+to n. (For Loop)
+5. Input a number and print its multiplication table (e.g., 5 × 1 = 5 ...
+5 × 10 = 50).
+6. Print the squares of all numbers from 1 to 10.
+7. Input an integer and count how many digits it has (using loop).
+8. Input a number (e.g., 1234) and print its reverse (4321).
+9. Find the sum of digits of a given number (e.g., 123 → 6).
+10. Calculate factorial of n using a for loop.
+11. Print all prime numbers between 1 and 100.
+ */
+package bookpractices;
+
+
+public class EvenNumbersBetweenOneToFifty {
+    public static void main(String[] args){
+        int i = 1;
+       
+        while(i <= 50){
+        if(i % 2 == 0){
+            System.out.println(i);
+        }
+        else{
+            System.out.println();
+        }
+        
+        i++;
+    }
+    }
+}

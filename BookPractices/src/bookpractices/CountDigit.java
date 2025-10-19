e9a371af4509eb24c090ed79995063dd81c5bb7e
Mohammad Ali <always.mohammad.ali@gmail.com>
2025-10-19 12:07:14 +0600

    Count digit of users' random number, reverse that number, and sum of the digits

---
+++ BookPractices/src/bookpractices/CountDigit.java
@@ -0,0 +1,43 @@
+/*
+ 7. Input an integer and count how many digits it has (using loop).
+8. Input a number (e.g., 1234) and print its reverse (4321).
+9. Find the sum of digits of a given number (e.g., 123 → 6).
+10. Calculate factorial of n using a for loop.
+11. Print all prime numbers between 1 and 100.
+ */
+
+package bookpractices;
+import java.util.Scanner;
+
+public class CountDigit {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter a random Number 'n' to get how many digits are there : ");
+        
+        int n = input.nextInt();
+        int original = n;
+        
+        int count = 0;
+        int reverse = 0;
+        int sum = 0;
+        
+        while(n != 0){
+            int digit = n % 10;
+            
+            count++;
+            reverse = reverse * 10 + digit;
+            sum += digit;
+            
+            n = n / 10;
+                  
+        }
+        System.out.println("The original no : " + original);
+        System.out.println("Total digit no : " + count );
+        System.out.println("The reverse no : " + reverse);
+        System.out.println("Total sum of the digit is : " + sum);
+       
+        input.close();
+        
+    }
+}
--- BookPractices/src/bookpractices/SquaresNumber.java
+++ BookPractices/src/bookpractices/SquaresNumber.java
@@ -1,10 +1,6 @@
 /*
  6. Print the squares of all numbers from 1 to 10.
-7. Input an integer and count how many digits it has (using loop).
-8. Input a number (e.g., 1234) and print its reverse (4321).
-9. Find the sum of digits of a given number (e.g., 123 → 6).
-10. Calculate factorial of n using a for loop.
-11. Print all prime numbers between 1 and 100.
+
  */
 package bookpractices;
 
@@ -16,6 +12,6 @@ public class SquaresNumber {
             int squareResult = i * i;
             System.out.println("Square of " + i + " = " + squareResult + "\n");
             i++;
-        }while(i <= 20);
+        }while(i <= 10);
     }
 }

5a8d6b259d82721ecce071e41f97c7527dc76fc0
Mohammad Ali <always.mohammad.ali@gmail.com>
2025-10-26 08:53:28 +0600

    convert a hex value to decimal

--- BookPractices/src/bookpractices/Hex2Decimal.java
+++ BookPractices/src/bookpractices/Hex2Decimal.java
@@ -1,13 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package bookpractices;
 
-/**
- *
- * @author shohe
- */
+package bookpractices;
+import java.util.Scanner;
+
+
 public class Hex2Decimal {
+    public static void main(String[] args){
+        
+       Scanner input =new Scanner(System.in);
+       
+       System.out.print("Enter a HexaDecimal value to convert it into Decimal: ");
+       String hex = input.nextLine();
+       
+       System.out.println("The hex value is : " + hex + " and its' decimal value is : "+ hexToDecimal(hex.toUpperCase()));
+       
+      
+       
+       
+       
+    }
+    public static int hexToDecimal(String hexValue){
+        int decimalValue = 0;
+        
+        for(int i = 0; i < hexValue.length() ; i++){
+            char hexChar = hexValue.charAt(i);
+            decimalValue = decimalValue * 16 + hexCharToDecimal(hexChar);
+        }
+        return decimalValue;
+    }
     
+    public static int hexCharToDecimal(char ch){
+        if(ch >= 'A' && ch <= 'F')
+            return 10 + ch - 'A';
+        
+        else
+            return ch - '0';
+        
+    }
 }
